81adb8ccff924a9892e5af3a6527a50972040bc9
Lucho-89 <lucho.deca@gmail.com>
2025-06-02 20:06:40 -0300

    Update Monitoreo Agrotech.sql

--- Monitoreo Agrotech.sql
+++ Monitoreo Agrotech.sql
@@ -1,20 +1,24 @@
+--Crear tabla cultivos
 CREATE TABLE Cultivos (
   IdCultivo INTEGER AUTO_INCREMENT PRIMARY KEY,
   Nombre VARCHAR(30) NOT NULL
 );
 
+--Crear tabla TipoSensores
 CREATE TABLE TipoSensores (
   IdTipoSensor INTEGER AUTO_INCREMENT PRIMARY KEY,
   Nombre VARCHAR(30) NOT NULL,
   Unidad VARCHAR(10) NOT NULL
 );
 
+--Crear tabla ubicaciones
 CREATE TABLE Ubicaciones (
   IdUbicacion INTEGER AUTO_INCREMENT PRIMARY KEY,
   Latitud DECIMAL(10,4) NOT NULL,
   Longitud DECIMAL(10,4) NOT NULL
 );
 
+--Crear tabla campos
 CREATE TABLE Campos (
   IdCampo INTEGER AUTO_INCREMENT PRIMARY KEY,
   Nombre VARCHAR(30) NOT NULL,
@@ -22,6 +26,7 @@ CREATE TABLE Campos (
   FOREIGN KEY (IdUbicacion) REFERENCES Ubicaciones(IdUbicacion)
 );
 
+--Crear tabla parcelas
 CREATE TABLE Parcelas (
   IdParcela INTEGER AUTO_INCREMENT PRIMARY KEY,
   Nombre VARCHAR (20) NOT NULL,
@@ -34,6 +39,7 @@ CREATE TABLE Parcelas (
   FOREIGN KEY (IdUbicacion) REFERENCES Ubicaciones(IdUbicacion)
 );
 
+--Crear tabla sensores
 CREATE TABLE Sensores (
   IdSensor INTEGER AUTO_INCREMENT PRIMARY KEY,
   Nombre VARCHAR(20) NOT NULL,
@@ -44,6 +50,7 @@ CREATE TABLE Sensores (
   Activado BOOLEAN DEFAULT TRUE
 );
 
+--Crear tabla mediciones
 CREATE TABLE Mediciones (
   IdMedicion INTEGER AUTO_INCREMENT PRIMARY KEY,
   IdParcela INTEGER,
@@ -55,16 +62,19 @@ CREATE TABLE Mediciones (
 );
 
 
+--Insertar datos tabla cultivos
 INSERT INTO Cultivos(Nombre) VALUES 
 ('TRIGO'),
 ('MAIZ'),
 ('SOJA');
 
+--Insertar datos en la tabla TipoSensores
 INSERT INTO TipoSensores(Nombre, Unidad) VALUES 
 ('HUMEDAD','%'),
 ('NIVEL PH','PH'),
 ('TEMPERATURA','°C');
 
+--Insertar datos en la tabla ubicaciones
 INSERT INTO Ubicaciones(Latitud, Longitud) VALUES 
 (-32.84105,-62.86593),
 (-32.84005,-62.86703),
@@ -76,11 +86,13 @@ INSERT INTO Ubicaciones(Latitud, Longitud) VALUES
 (-33.3842,-64.7228),
 (-33.3842,-64.7228);
 
+--Insertar datos en la tabla campos
 INSERT INTO Campos(Nombre, IdUbicacion) VALUES 
 ('Decandido',1),
 ('Coronel',5),
 ('Elguero',8);
 
+--Insertar datos en la tabla parcelas
 INSERT INTO Parcelas(Nombre,IdCampo,IdCultivo,Has,IdUbicacion) VALUES 
 ('Norte',1,2,30,2),
 ('Centro',1,1,25.82,3),
@@ -89,6 +101,7 @@ INSERT INTO Parcelas(Nombre,IdCampo,IdCultivo,Has,IdUbicacion) VALUES
 (2,2,3,76.34,7),
 ('Unica',3,1,100,9);
 
+--Insertar datos en la tabla sensores
 INSERT INTO Sensores(Nombre,IdTipoSensor,IdUbicacion,Activado) VALUES 
 ('ROJO',1,2,TRUE),
 ('AZUL',2,2,FALSE),
@@ -105,6 +118,7 @@ INSERT INTO Sensores(Nombre,IdTipoSensor,IdUbicacion,Activado) VALUES
 ('PH',2,9,TRUE),
 ('TEMPERATURA',3,9,TRUE);
 
+--Insertar datos en la tabla mediciones
 INSERT INTO Mediciones (IdParcela, IdSensor, Dato, Fecha) VALUES 
 (1, 1, 25.75, '2025-05-31'),
 (1, 1, 80.74, '2025-06-01'),
@@ -120,26 +134,33 @@ INSERT INTO Mediciones (IdParcela, IdSensor, Dato, Fecha) VALUES
 (6, 12, 80.74, '2025-06-01'),
 (6, 13, 28.15, '2025-06-02');
 
+--Mostrar todas las parcelas
 SELECT * FROM Parcelas
 
+--Mostrar los nombres de las parcelas del campo que contenga IdCampo = 1
 SELECT Nombre FROM Parcelas WHERE IdCampo = 1
 
+--Mostrar nombres e IdTipoSensor de los sensores que se encuentren activados
 SELECT Nombre,IdTipoSensor FROM Sensores WHERE Activado = TRUE
 
-SELECT s.nombre AS SENSOR,
-    ts.nombre AS TIPO_SENSOR,
-    ts.unidad AS UNIDAD
+/*Mostrar el nombre del campo y nombre de la parcela donde hay sembrado maiz*/
+SELECT c.nombre AS NOMBRE_CAMPO,
+       p.Nombre AS NOMBRE_PARCELA
 FROM
-    Sensores s
+    Campos c
 JOIN
-    TipoSensores ts ON s.IdTipoSensor = ts.IdTipoSensor
+    Parcelas p ON c.IdCampo = p.IdCampo
+JOIN
+    Cultivos cu ON p.IdCultivo = cu.IdCultivo
 WHERE
-    ts.nombre = 'HUMEDAD';
-
-SELECT c.Nombre AS NOMBRE_CAMPO,
-    p.Nombre AS NOMBRE_PARCELA,
+    cu.nombre = 'MAIZ';
+    
+/*Mostrar nombre de las parcelas, nombre del sensor, dato de la medicion, unidad del dato, 
+fecha de la medicion de los sensores que miden humedad en el campo de Decandido*/
+SELECT p.Nombre AS NOMBRE_PARCELA,
     s.Nombre AS NOMBRE_SENSOR,
     m.Dato AS DATO,
+    ts.Unidad as UNIDAD,
     m.Fecha as FECHA
 FROM
     Mediciones m
@@ -152,4 +173,4 @@ JOIN
 JOIN
     TipoSensores ts ON s.IdTipoSensor = ts.IdTipoSensor
 WHERE
-    ts.Nombre = 'HUMEDAD'
+    ts.Nombre = 'HUMEDAD' AND c.Nombre = 'Decandido'
